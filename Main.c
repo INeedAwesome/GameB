69989862ea983c4ef30c5b164885f7a0590317d3
Mattis Kjellerås <mattis@gr8.site>
2022-02-25 19:48:40 +0100

    Updated Main.c
    Fullscreen

--- Main.c
+++ Main.c
@@ -6,7 +6,12 @@ BOOL gGameIsRunning;
 
 void* Memory;
 	
-GAMEBITMAP gDrawingSurface;
+GAMEBITMAP gBackBuffer;
+
+MONITORINFO gMonitorInfo = { sizeof(MONITORINFO) } ;
+
+int32_t gMonitorWidth;
+int32_t gMonitorHeight;
 
 int WINAPI WinMain(HINSTANCE Instance, HINSTANCE PrevInstance, PSTR CommandLine, INT CmdShowWindow)
 {
@@ -15,31 +20,34 @@ int WINAPI WinMain(HINSTANCE Instance, HINSTANCE PrevInstance, PSTR CommandLine,
 		MessageBoxA(NULL, "Another instance is already running!", "Error!", MB_ICONEXCLAMATION | MB_OK);
 		goto Exit;
 	}
-
+	
 	if (CreateMainGameWindow() != ERROR_SUCCESS)
 	{
 		MessageBoxA(NULL, "Failed to create window! ", "Error!", MB_ICONEXCLAMATION | MB_OK);
 		goto Exit;
 	}
 
-	gDrawingSurface.BitmapInfo.bmiHeader.biSize = sizeof(gDrawingSurface.BitmapInfo.bmiHeader);
-	gDrawingSurface.BitmapInfo.bmiHeader.biWidth = GAME_RES_WIDTH;
-	gDrawingSurface.BitmapInfo.bmiHeader.biHeight = GAME_RES_HEIGHT;
-	gDrawingSurface.BitmapInfo.bmiHeader.biBitCount = GAME_BPP;
-	gDrawingSurface.BitmapInfo.bmiHeader.biCompression = BI_RGB;
-	gDrawingSurface.BitmapInfo.bmiHeader.biPlanes = 1;
-	if ((gDrawingSurface.Memory = VirtualAlloc(NULL, GAME_DRAWING_AREA_MEMORY_SIZE, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE)) == NULL)
+	gBackBuffer.BitmapInfo.bmiHeader.biSize = sizeof(gBackBuffer.BitmapInfo.bmiHeader);
+	gBackBuffer.BitmapInfo.bmiHeader.biWidth = GAME_RES_WIDTH;
+	gBackBuffer.BitmapInfo.bmiHeader.biHeight = GAME_RES_HEIGHT;
+	gBackBuffer.BitmapInfo.bmiHeader.biBitCount = GAME_BPP;
+	gBackBuffer.BitmapInfo.bmiHeader.biCompression = BI_RGB;
+	gBackBuffer.BitmapInfo.bmiHeader.biPlanes = 1;
+	gBackBuffer.Memory = VirtualAlloc(NULL, GAME_DRAWING_AREA_MEMORY_SIZE, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
+	if (gBackBuffer.Memory == NULL)
 	{
 		MessageBoxA(NULL, "Failed to allocate memory for drawing surface! ", "Error!", MB_ICONEXCLAMATION | MB_OK);
 		goto Exit;
 	}
 
+	memset(gBackBuffer.Memory, 0x7F, GAME_DRAWING_AREA_MEMORY_SIZE);
+
 	gGameIsRunning = TRUE;
 	MSG Message = { 0 };
 	
-	while (gGameIsRunning)
-	{
-		while (PeekMessageA(&Message, gGameWindow, 0, 0, PM_REMOVE))
+	while (gGameIsRunning) 
+	{ 
+		while (PeekMessageA(&Message, gGameWindow, 0, 0, PM_REMOVE)) 
 		{
 			DispatchMessageA(&Message);
 		}
@@ -69,7 +77,30 @@ void ProcessPlayerInput(void)
 
 void RenderFrameGraphics(void)
 {
+	//memset(gBackBuffer.Memory, 0xFF, GAME_RES_HEIGHT * GAME_RES_WIDTH * 4);
 
+	PIXEL32 Pixel = { 0 };
+	Pixel.Blue = 0xff;
+	Pixel.Green = 0;
+	Pixel.Red = 0;
+	Pixel.Alpha = 0xff;
+
+	for (int x = 0; x < GAME_RES_HEIGHT* GAME_RES_WIDTH; x++)
+	{
+		memcpy((PIXEL32*)gBackBuffer.Memory + x, & Pixel, 4, sizeof(PIXEL32));
+	}
+
+
+	HDC DeviceContext = GetDC(gGameWindow);
+
+	StretchDIBits(DeviceContext, 0, 0, 
+		gMonitorWidth, gMonitorHeight - 2, 
+		0, 0, 
+		GAME_RES_WIDTH, GAME_RES_HEIGHT, 
+		gBackBuffer.Memory, &gBackBuffer.BitmapInfo,
+		DIB_RGB_COLORS, SRCCOPY);
+
+	ReleaseDC(gGameWindow, DeviceContext);
 }
 
 LRESULT CALLBACK MainWindowProcedure(_In_ HWND WindowHandle, _In_ UINT Message, _In_ WPARAM WideParameter, _In_ LPARAM LongParameter)
@@ -99,6 +130,7 @@ DWORD CreateMainGameWindow(void)
 
 	WNDCLASSEXA WindowClass = { 0 };
 
+
 	WindowClass.cbSize = sizeof(WNDCLASSEXA);
 	WindowClass.style = 0;
 	WindowClass.lpfnWndProc = MainWindowProcedure;
@@ -108,7 +140,7 @@ DWORD CreateMainGameWindow(void)
 	WindowClass.hIcon = LoadIconA(NULL, IDI_APPLICATION);
 	WindowClass.hIconSm = LoadIconA(NULL, IDI_APPLICATION);
 	WindowClass.hCursor = LoadCursorA(NULL, IDC_ARROW);
-	WindowClass.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	WindowClass.hbrBackground = CreateSolidBrush(RGB(255, 0, 255));
 	WindowClass.lpszMenuName = NULL;
 	WindowClass.lpszClassName = GAME_NAME "_WINDOWCLASS";
 
@@ -120,18 +152,39 @@ DWORD CreateMainGameWindow(void)
 	}
 
 	gGameWindow = CreateWindowExA(WS_EX_CLIENTEDGE, WindowClass.lpszClassName, GAME_NAME,
-		WS_OVERLAPPEDWINDOW | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT, 1280, 720, NULL, NULL, GetModuleHandleA(NULL), NULL);
+		WS_OVERLAPPEDWINDOW | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT, 
+		GAME_RES_WIDTH, GAME_RES_HEIGHT, NULL, NULL, GetModuleHandleA(NULL), NULL);
 
 	if (gGameWindow == NULL)
 	{
 		Result = GetLastError();
-		MessageBoxA(NULL, "  Window Creation Failed!  ", "Error!", MB_ICONEXCLAMATION | MB_OK);
+		MessageBoxA(NULL, "Window Creation Failed!", "Error!", MB_ICONEXCLAMATION | MB_OK);
 		goto Exit;
 	}
 
+	if (GetMonitorInfoA(MonitorFromWindow(gGameWindow, MONITOR_DEFAULTTOPRIMARY), &gMonitorInfo) == FALSE)
+	{
+		Result = ERROR_MONITOR_INFO_FAILED;
+		goto Exit;
+	}
+
+	gMonitorWidth = gMonitorInfo.rcMonitor.right - gMonitorInfo.rcMonitor.left;
+	gMonitorHeight = gMonitorInfo.rcMonitor.bottom - gMonitorInfo.rcMonitor.top;
+
+	if (SetWindowLongPtrA(gGameWindow, GWL_STYLE, (WS_OVERLAPPEDWINDOW | WS_VISIBLE) & ~WS_OVERLAPPEDWINDOW) == 0)//  taking away the WS_OVERLAPPEDWINDOW except WS_VISIBLE
+	{
+		Result = GetLastError();
+		goto Exit;
+	}
+	if (SetWindowPos(gGameWindow, HWND_TOP, gMonitorInfo.rcMonitor.left, gMonitorInfo.rcMonitor.top, gMonitorWidth, gMonitorHeight, SWP_NOOWNERZORDER | SWP_FRAMECHANGED) == 0)
+	{
+		Result = GetLastError();
+		goto Exit;
+	}
+
+	
 
 Exit:
-
 	return Result;
 }
 
--- Main.h
+++ Main.h
@@ -2,15 +2,26 @@
 
 #include <stdio.h>
 #include <Windows.h>
+#include <stdint.h>
 
 #define GAME_NAME		"Game_B"
 #define GAME_RES_WIDTH  384
-#define GAME_RES_HEIGHT 216
+#define GAME_RES_HEIGHT 240
 #define GAME_BPP		32
 #define GAME_DRAWING_AREA_MEMORY_SIZE	(GAME_RES_WIDTH * GAME_RES_HEIGHT * (32 / 8))
 
-#define ERROR_OUT_OF_MEMORY 5
-#define ERROR_ 1
+#define ERROR_OK 0
+#define ERROR 1
+#define ERROR_OUT_OF_MEMORY 20
+#define ERROR_MONITOR_INFO_FAILED 10
+
+typedef struct PIXEL32
+{
+	uint8_t Blue; // uint8_t defines unsigned char
+	uint8_t Green;
+	uint8_t Red;
+	uint8_t Alpha;
+} PIXEL32; // 8 bits each = 32
 
 typedef struct GAMEBITMAP
 {
